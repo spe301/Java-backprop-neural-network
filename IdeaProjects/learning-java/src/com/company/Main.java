723b4b096673457f648ff14bcf73c512235b3f75
Spencer Holley <aacjpw@gmail.com>
2021-05-17 17:17:44 -0500

    added code file

---
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int lilBabyAge = 26;
+        int keshaAge = 34;
+        boolean keshaIsOlder = keshaAge > lilBabyAge;
+        System.out.println(keshaIsOlder);
+    }
+}
+
+// public -
+// void -
+// final -
+// class -
