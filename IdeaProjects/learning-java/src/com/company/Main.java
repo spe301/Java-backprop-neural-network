cbafcfab54cf7d75a08620db15f6a476a8703898
Spencer Holley <aacjpw@gmail.com>
2021-05-17 20:03:43 -0500

    still learning basics

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -2,20 +2,25 @@ package com.company;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-        int lilBabyAge = 26;
-        int keshaAge = 34;
-        boolean keshaIsOlder = keshaAge > lilBabyAge;
-        System.out.println(keshaIsOlder);
+        int [] temps = {66, 67, 68, 58, 78, 64, 75, 76, 67, 57};
+        for (int i = 0; i < temps.length; i+=2) {
+            System.out.println(temps[i]);
+        }
     }
 }
 
 // public -
 // void -
 // final -
+// && - and
+// || - or
 // class -
+// else is followed by a {}
+// use switch when preforming selections on one value, no ands
