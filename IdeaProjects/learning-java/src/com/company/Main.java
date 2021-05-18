09ec1f54ff54957e1530992ec95ce29a81c4e49c
Spencer Holley <aacjpw@gmail.com>
2021-05-18 16:59:10 -0500

    got through beginner tutorial

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -5,16 +5,33 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int [] temps = {66, 67, 68, 58, 78, 64, 75, 76, 67, 57};
-        for (int i = 0; i < temps.length; i+=2) {
-            System.out.println(temps[i]);
-        }
+        Person Antoine = new Person("Black", 212,true, "Nashville");
+        Person Wu = new Person("Asian", 133, false, "Singapore");
+        Person Chad = new Person("White", 158, false, "Los Angeles");
+        System.out.println(Antoine.married);
+    }
+
+    static class Person {
+        String race;
+        int weight;
+        boolean married;
+        String city;
+
+        Person(String race, int weight, boolean married, String city){
+            this.race = race;
+            this.weight = weight;
+            this.married = married;
+            this.city = city;
+        };
     }
 }
+
+
 
 // public -
 // void -
