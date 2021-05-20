398677314927f7dbb1e70a2eeb13bfa431d75189
Spencer Holley <aacjpw@gmail.com>
2021-05-20 09:54:37 -0500

    started synaptic weights

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -1,22 +1,29 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
     int [][] X = {{0,0,1}, {0,1,1}, {1,0,1}, {1,1,1}};
     int [][] y = {{0}, {0}, {1}, {1}};
+        System.out.println(generateRandom(3, 4));
     }
 
-    public static float sigmoid(int [] [] X){
-        return 1/(1+Math.exp(X));
+    //public static int [][] sigmoid(int [][] X, boolean deriv){
+        //if(deriv == false){
+            //return X * (1-X);}
+        //return 1/(1+Math.exp(-X));
+        //}
+
+    public static List<List<Float>> generateRandom(int col, int row) {
+        List<List<Float>> dataset = new ArrayList<>();
+        for (int i = 0; i < row; i++){
+        List<Float> column = new ArrayList<Float>();
+        for (int j = 0; j < col; j++){
+            column.add((float) Math.random());
+        }dataset.add(column);}
+        return dataset;
     }
 }
 
