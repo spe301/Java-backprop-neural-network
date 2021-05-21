48b355502ed9c98d1a178aee1000f3a2e14af7e6
Spencer Holley <aacjpw@gmail.com>
2021-05-21 16:59:36 -0500

    finished weights

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -7,21 +7,24 @@ public class Main {
     public static void main(String[] args) {
     int [][] X = {{0,0,1}, {0,1,1}, {1,0,1}, {1,1,1}};
     int [][] y = {{0}, {0}, {1}, {1}};
-        System.out.println(generateRandom(3, 4));
+    List<List<Float>> syn0 = generateWeights(4, 3, 2, 1);
+    List<List<Float>> syn1 = generateWeights(4, 1, 2, 1);
+    //for (int i = 0; i < 60000; i++){ int [][] l0 = X; }
+        System.out.println(sigmoid((float) 0.78, false));
     }
 
-    //public static int [][] sigmoid(int [][] X, boolean deriv){
-        //if(deriv == false){
-            //return X * (1-X);}
-        //return 1/(1+Math.exp(-X));
-        //}
-
-    public static List<List<Float>> generateRandom(int col, int row) {
+    public static float sigmoid(float X, boolean deriv){
+        if (deriv == true){
+            return X * (1-X);
+        }
+        return (float) (1/(1+Math.exp(-1*X)));
+    }
+    public static List<List<Float>> generateWeights(int col, int row, int factor, int bias) {
         List<List<Float>> dataset = new ArrayList<>();
         for (int i = 0; i < row; i++){
         List<Float> column = new ArrayList<Float>();
         for (int j = 0; j < col; j++){
-            column.add((float) Math.random());
+            column.add((float) (Math.random()*factor)-bias);
         }dataset.add(column);}
         return dataset;
     }
