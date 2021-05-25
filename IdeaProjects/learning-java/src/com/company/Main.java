7682cd750f975e7529e972f218b4b8f4d4e1b9d5
Spencer Holley <aacjpw@gmail.com>
2021-05-25 17:04:22 -0500

    learned basic java methods

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -1,35 +1,17 @@
 package com.company;
 
 import java.util.*;
-
 public class Main {
-
-    public static void main(String[] args) {
-    double[] workHours = {4, 4, 8, 8, 8.5, 9, 10, 10, 12, 12, 13, 10, 14, 8, 9, 8, 6, 9};
-    double [] socialHours = {0, 9, 5, 6, 4, 4, 2, 1, 0, 1, 3, 4, 0, 5, 3.5, 5, 5.5, 3};
-        //System.out.println(error(workHours, socialHours));
-        System.out.println(workHours[17]);
-        System.out.println(socialHours[17]);
+    public static void main(String[] args){
+        Array numbers = new Array(3);
+        numbers.insert(60);
+        numbers.insert(120);
+        numbers.insert(180);
+        numbers.insert(240);
+        System.out.println(numbers.indexOf(600));
+        numbers.print();
     }
-
-    public static double predict(double X){
-        return (X * -1) + 13;
-    }
-
-    public static double error(double[] X, double[]y){
-        double Error = 0;
-        for (int i = 0; i < X.length; i++){
-            double yHat = predict(X[i]);
-            double delta = (yHat - y[i])*(yHat - y[i]);
-            Error += delta;
-            ;
-        }
-        return Error/ X.length;
-    }
-
-
 }
-
 
 
 // public -
