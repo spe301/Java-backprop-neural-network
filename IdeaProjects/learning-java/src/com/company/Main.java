e99065355aad84d52d324c73fbd9a644089cf495
Spencer Holley <aacjpw@gmail.com>
2021-05-27 17:03:38 -0500

    __

--- IdeaProjects/learning-java/src/com/company/Main.java
+++ IdeaProjects/learning-java/src/com/company/Main.java
@@ -3,7 +3,16 @@ package com.company;
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-
+        ArrayList<Integer> heights = new ArrayList<Integer>();
+        heights.add(73);
+        heights.add(66);
+        heights.add(69);
+        heights.add(67);
+        heights.add(69);
+        heights.add(70);
+        heights.add(74);
+        heights.add(70);
+        System.out.println(heights.get(3));
     }
 }
 
